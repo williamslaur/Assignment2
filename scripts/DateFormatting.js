7763c09d3dd661ba75a5f61bc380440ebb1bcc1b
williamslaur <williamslaur@yahoo.com>
2018-04-02 21:48:28 -0500

    all

--- scripts/DateFormatting.js
+++ scripts/DateFormatting.js
@@ -1,5 +1,4 @@
-var DateFormatter = {}
-debugger
+var DateFormatter = {
 
 /* Example
 getMonthYear: function(date){
@@ -15,46 +14,34 @@ console.log('Get Month Year ' + getMonthYear);
 
 
 //returns returns a the date's time with hours, minutes and AM or PM  
-getShortTime: function(){
+getShortTime: function(date){
     var hour = time.getHours();
     var minutes = time.getMinutes();
-    return hour + '/' +  minutes;
-}
-var getShortTime = DateFormatter.getShortTime(new Date());
-console.log('Get Short Time ' + getShortTime);
-
-
+    var temp = (hour > 12) ? ' PM' : ' AM';
+    return hour + ':' +  minutes + temp;
+},
 //returns a the date's time with hours, minutes, seconds and AM or PM getLongTime
 getLongTime: function(date){
     var hours = time.gethours();
     var minutes = time.getMinutes();
     var seconds = time.getSeconds();
-    return hours + ", " + minutes + ", " + seconds; 
-}
-var getLongTime = DateFormatter.getLongTime(new Date());
-console.log('Get Long Time ' + getLongTime);
-
-
+    var temp = (hour > 12) ? ' PM' : ' AM';
+    return hours + ", " + minutes + ", " + seconds + temp; 
+},
 //returns the date in the following format month/day/year getShortDate
 getShortDate: function(date){
     var month = date.getMonth();
     var day = date.getDate();
     var year = date.getFullYear();
-    return month + "/ " + day + "/ " + year;
-}
-var getShortDate = DateFormatter.getShortDate(new Date());
-console.log('Get Short Date ' + getShortDate);
-
-
+    return month + "/" + day + "/" + year;
+},
 //returns the date in the following format Month Day, Year - getLongDate
 getLongDate: function(date){
     var month = date.getMonth();
     var day = date.getDate();
     var year = date.getFullYear();
     return month + " " + day + ", " + year;
-}
-var getLongDate = DateFormatter.getLongDate(new Date());
-console.log('Get Long Date ' + getLongDate);
+},
 
 //returns the date in the following format month/day/year hour:minutes AM|PM - getShortDateTime
 getShortDateTime: function(date){
@@ -63,10 +50,9 @@ getShortDateTime: function(date){
     var year = date.getFullYear();
     var hour = time.getHours();
     var minutes = time.getMinutes();
-    return month + "/" + day + "/" + year + " " + hour + ":" + minutes;
-}
-var getLongDate = DateFormatter.getShortDateTime(new Date());
-console.log('Get Short Date Time ' + getShortDateTime);
+    var temp = (hour > 12) ? ' PM' : ' AM';
+    return month + "/" + day + "/" + year + " " + hour + ":" + minutes + temp;
+},
 
 //returns the date in the following format- Month Day, Year hour:minutes AM|PM
 getLongDateTime: function(date){
@@ -75,10 +61,9 @@ getLongDateTime: function(date){
     var year = date.getFullYear();
     var hour = time.getHours();
     var minutes = time.getMinutes();
-    return month + " " + day + ", " + year + " " + hour + ":" + minutes;
-}
-var getLongDateTime = DateFormatter.getLongDateTime(new Date());
-console.log('Get Long Date Time ' + getLongDateTime);
+    var temp = (hour > 12) ? ' PM' : ' AM';
+    return month + " " + day + ", " + year + " " + hour + ":" + minutes + temp;
+},
 
 //returns the date in the following format- Day of the week, Month Day, Year hour:minutes AM|PM
 getExtendedDateTime: function(date){
@@ -89,8 +74,14 @@ getExtendedDateTime: function(date){
     var hour = time.getHours();
     var minutes = time.getMinutes();
     return day + " " + month + " " + date + ", " + year + " " + hour + ":" + minutes;
-}
-var getExtendedDateTime = DateFormatter.getExtendedDateTime(new Date());
-console.log('Get Long Date Time ' + getExtendedDateTime);
+},
 
-
+//data for console.log
+var date = new Date();
+console.log(DateFormatter.getShortTime(date)),
+console.log(DateFormatter.getLongTime(date)),
+console.log(DateFormatter.getShortDate(date)),
+console.log(DateFormatter.getLongDate(date)),
+console.log(DateFormatter.getShortDateTime(date)),
+console.log(DateFormatter.getLongDateTime(date)),
+console.log(DateFormatter.getExtendedDateTime(date))
